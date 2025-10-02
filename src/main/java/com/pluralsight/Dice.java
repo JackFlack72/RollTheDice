ae562877b50189756946559465c0c3c32235e165
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-10-02 16:02:33 -0500

    first commit

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -3,10 +3,14 @@ import java.util.*;
 
 public class Dice {
     public static void main(String[] args) {
+        Dice dice = new Dice();
+
+        int roll1;
+        int roll2;
 
     }
 
-    public static roll() {
-
+    public static void roll(int num) {
+        num = (int)(Math.random() * 6) + 1;
     }
 }
